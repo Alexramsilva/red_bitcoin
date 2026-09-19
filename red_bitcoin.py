42b1718433be97d418b9a3ebcca0e7dad565a6a5
UNRC_Hub <106453707+Alexramsilva@users.noreply.github.com>
2026-09-19 11:45:53 -0600

    Add header for Bitcoin Trading Opportunity Predictor

--- red_bitcoin.py
+++ red_bitcoin.py
@@ -215,4 +215,7 @@ st.markdown("""
     }
 </style>
 """, unsafe_allow_html=True)
-
+st.markdown(
+    '<h1 style="color:#FFFFFF;">Bitcoin Trading Opportunity Predictor</h1>',
+    unsafe_allow_html=True
+)
